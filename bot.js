8685c71fe0a9896363e2abde6e73246d42daaef7
Dolphin-Bunny <49925062+Dolphin-Bunny@users.noreply.github.com>
2020-10-02 11:24:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', message => {
 
     if (message.content === 'ping') {
 
-       message.reply('pong\n\n`If you are seeing this the bot is hosted properly on Heroku');
+       message.reply('pong\n\n`If you are seeing this the bot is hosted properly on Heroku`');
 
        }
 
